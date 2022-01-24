528973ed41d5ef04fbeefe7abd1d4de113ada089
Sam Gwilym <gwilym@me.com>
2022-01-24 15:28:33 +0000

    Add viewport tag to page head

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,7 @@ function Document({
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.png" type="image/png" />
         {title ? <title>{title}</title> : null}
         <Meta />
