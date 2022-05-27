618f4efb7d24a28ca71c5af99a11a79dd8bb5911
Sam Gwilym <gwilym@me.com>
2022-05-27 13:41:33 +0200

    Update latest Earthstar JS ver to 9.3.3

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-export const LATEST_EARTHSTAR_VERSION = "9.3.2";
+export const LATEST_EARTHSTAR_VERSION = "9.3.3";
 export const LATEST_DOCS_URL =
   `https://doc.deno.land/https://deno.land/x/earthstar@v${LATEST_EARTHSTAR_VERSION}/mod.ts`;
 
