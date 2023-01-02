3e5b2b8fb877281e17679e59b0515bcbc6f3ef1b
Sam Gwilym <sam@gwil.garden>
2023-01-02 12:32:44 +0000

    Use v10 URLs

--- app/components/module-inject.tsx
+++ app/components/module-inject.tsx
@@ -6,7 +6,7 @@ export default function ModuleInject() {
       type="module"
       dangerouslySetInnerHTML={{
         __html:
-          `import * as Earthstar from "https://cdn.earthstar-project.org/js/earthstar.web.v10.0.0-rc.1.js";
+          `import * as Earthstar from "https://cdn.earthstar-project.org/js/earthstar.web.v10.0.0.js";
 				
 				globalThis.Earthstar = Earthstar;`,
       }}
