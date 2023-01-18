a29c34fb7efc1fa5a461ef35798b99729f5972e0
Sam Gwilym <sam@gwil.garden>
2023-01-18 11:13:13 +0000

    Update version numbers

--- app/constants.ts
+++ app/constants.ts
@@ -1,3 +1,3 @@
-export const LATEST_EARTHSTAR_VERSION = "v10.0.0";
+export const LATEST_EARTHSTAR_VERSION = "v10.0.1";
 export const LATEST_DOCS_URL =
   `https://doc.deno.land/https://deno.land/x/earthstar@v${LATEST_EARTHSTAR_VERSION}/mod.ts`;
